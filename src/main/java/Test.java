6642a541157d9362b64c394c6493ec91a488bffc
ThejanRupasinghe <thejanrupasinghe.14@cse.mrt.ac.lk>
2017-07-18 12:16:55 +0530

    deleted file.txt

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -54,29 +54,6 @@ public class Test {
     public void testMethod () {
         JSONParser parser = new JSONParser();
 
-        try {
-
-            Object obj = parser.parse(new FileReader("file.txt"));
-
-            JSONObject jsonObject = (JSONObject) obj;
-
-            String name = (String) jsonObject.get("Name");
-            String author = (String) jsonObject.get("Author");
-            JSONArray companyList = (JSONArray) jsonObject.get("Company List");
-
-            System.out.println("Name: " + name);
-            System.out.println("Author: " + author);
-            System.out.println("\nCompany List:");
-            System.out.println(companyList.get(1).getClass().getName());
-            Iterator<String> iterator = companyList.iterator();
-            while (iterator.hasNext()) {
-                System.out.println(iterator.next());
-            }
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
         /** Taking Date in the format logstash-2017.07.12 **/
         DateFormat dateFormat = new SimpleDateFormat("yyyy.MM.dd");
         Date date = new Date();
